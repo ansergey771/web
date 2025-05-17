01d84ea45db0026966591c587ef7f38ccaf1172b
ansergey771 <126673699+ansergey771@users.noreply.github.com>
2025-05-17 09:32:42 +0300

    Update factory_use.php

--- lab3/patterns/factory_use.php
+++ lab3/patterns/factory_use.php
@@ -1,41 +1,7 @@
 <?php
+spl_autoload_register();
 
-require_once 'UserFactory.php';
+use Factory\Router;
 
-// Загрузка данных пользователей
-$usersData = require_once 'users.php';
-
-// Создание фабрики пользователей
-$userFactory = new ConcreteUserFactory();
-
-// Вывод информации обо всех пользователях
-echo '<pre>';
-echo "Информация о пользователях:\n";
-echo "===========================\n";
-echo '</pre>';
-
-foreach ($usersData as $userData) {
-    // Создание объекта пользователя с помощью фабрики
-    $user = $userFactory->getUser($userData);
-    
-    // Вывод информации о пользователе
-    echo '<pre>';
-    echo $user->getInfo() . "\n";
-    echo '</pre>';
-}
-
-// Демонстрация использования фабрики для конкретного типа пользователя
-echo '<pre>';
-echo "\nДоступ к свойствам конкретного пользователя:\n";
-echo "===========================================\n";
-echo '</pre>';
-
-$adminData = $usersData[0]; // Получение данных первого пользователя (администратора)
-$admin = $userFactory->getUser($adminData);
-
-echo '<pre>';
-echo "ID пользователя: " . $admin->getId() . "\n";
-echo "Имя пользователя: " . $admin->getName() . "\n";
-echo "Email пользователя: " . $admin->getEmail() . "\n";
-echo "Тип пользователя: " . $admin->getType() . "\n"; 
-echo '</pre>';
+$obj = Router::parse('users');
+echo $obj->render();
