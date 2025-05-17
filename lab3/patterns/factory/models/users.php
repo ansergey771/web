bb4cdfbeb196f6d0b3f1ba9326a620cff075b29e
ansergey771 <126673699+ansergey771@users.noreply.github.com>
2025-05-17 09:41:20 +0300

    Update users.php

--- lab3/patterns/factory/models/users.php
+++ lab3/patterns/factory/models/users.php
@@ -6,13 +6,36 @@ class Users extends Collection
     public function __construct(public ?array $users = null)
     {
         $users ??= [
-            new User('dmitry.koterov@gmail.com', 'password', 'Дмитрий', 'Котеров'),
-            new User('igorsimdyanov@gmail.com', 'password', 'Игорь', 'Симдянов'),
-            new User('oleg.ivanov@gmail.com', 'password', 'Олег', 'Иванов'),
-            new User('ivan.smirnov@gmail.com', 'password', 'Иван', 'Смирнов'),
-            new User('olga.sidorova@gmail.com', 'password', 'Ольга', 'Сидорова')
+            new User(
+                "anna.petrovna@gmail.com",
+                "password",
+                "Анна",
+                "Петрова"),
+            new User(
+                "sergey.volkov@mail.ru",
+                "password",
+                "Сергей",
+                "Волков"),
+            new User(
+                "maria.ivanova@yandex.ru",
+                "password",
+                "Мария",
+                "Иванова"),
+            new User(
+                "daniil.kuznetsov@gmail.com",
+                "password",
+                "Даниил",
+                "Кузнецов"),
+            new User(
+                "elena.smirnova@outlook.com",
+                "password",
+                "Елена",
+                "Смирнова"),
+            new User(
+                "andrey.egorov@gmail.com",
+                "password",
+                "Андрей",)
         ];
         parent::__construct($users);
     }
 }
-?>
